796d695068b5b0d2f77c4328163f38f6c75ee5c4
mitrenegade <bobbyren@gmail.com>
2014-11-11 10:36:14 -0500

    updated Parse library; updated parse App idx
    updated parse libraries to 1.5

--- BabyGems/AppDelegate.m
+++ BabyGems/AppDelegate.m
@@ -17,8 +17,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    [Parse setApplicationId:@"HBwUDQrOSWLhK7Lx2yNYviQ6xe8sJHeBMkpoX3Mm"
-                  clientKey:@"Oqu48KsSu2fg8SFEJjoAElCIqaSPDpPqxW5QceBM"];
+    [Parse setApplicationId:@"7ed10Q7iOMBLppi3FXzApRhmaQxsJdXlS8sbBbaN"
+                  clientKey:@"re1mhkjLyyv31TdzwqlvbPiSIIKMUBjXdll5e1Nw"];
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
 
     PFUser *user = [PFUser currentUser];
